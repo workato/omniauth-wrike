edb7792100a255d55861098a6f08e3453bfd02c5
Juan Puelpan <jpuelpan528@gmail.com>
2016-03-17 14:06:09 -0300

    Update raw_info method

--- lib/omniauth/strategies/wrike.rb
+++ lib/omniauth/strategies/wrike.rb
@@ -9,10 +9,6 @@ module OmniAuth
         :token_url => 'https://www.wrike.com/oauth2/token'
       }
 
-      # def request_phase
-      #   super
-      # end
-
       # def authorize_params
         # super.tap do |params|
         #   %w[scope client_options].each do |v|
@@ -23,29 +19,28 @@ module OmniAuth
         # end
       # end
 
-      # uid { raw_info['uid'] }
+      uid { raw_info['id'] }
 
-      # info do
-      #   {
-      #     'nickname' => raw_info['login'],
-      #     'email' => primary_email,
-      #     'name' => raw_info['name'],
-      #     'image' => raw_info['avatar_url'],
-      #     'urls' => {
-      #       'GitHub' => raw_info['html_url'],
-      #       'Blog' => raw_info['blog'],
-      #     },
-      #   }
-      # end
+      info do
+        {
+          'uid' => raw_info['id'],
+          'name' => raw_info['name'],
+          'email' => nil,
+          'image' => nil
+        }
+      end
 
-      # extra do
-      #   {:raw_info => raw_info, :all_emails => emails}
-      # end
+      def raw_info
+        @raw_info ||= default_account
+      end
 
-      # def raw_info
-      #   access_token.options[:mode] = :query
-      #   @raw_info ||= access_token.get('user').parsed
-      # end
+      def accounts
+        @accounts ||= access_token.get('/accounts').parsed['data']
+      end
+
+      def default_account
+        @default_account ||= accounts.first
+      end
 
       # def email
       #   (email_access_allowed?) ? primary_email : raw_info['email']
