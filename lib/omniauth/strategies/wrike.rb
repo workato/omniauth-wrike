023f9687b90beaa85a5e1214673b8ea641f5fb3e
Pavel Perkin <pavlo.perkin@workato.com>
2018-11-12 17:37:54 +0200

    Use provided host for user data request

--- lib/omniauth/strategies/wrike.rb
+++ lib/omniauth/strategies/wrike.rb
@@ -31,8 +31,12 @@ module OmniAuth
         raw_info['profiles'].first['email']
       end
 
+      def user_data_url
+        @url ||= URI::HTTPS.build(host: access_token.params['host'], path: '/api/v4/contacts', query: 'me=true').to_s
+      end
+
       def raw_info
-        @raw_info ||= access_token.get('contacts?me=true').parsed['data'].first
+        @raw_info ||= access_token.get(user_data_url).parsed['data'].first
       end
     end
   end
